bbe9021c5e3eea51b4398db41fae2c7e8654c58c
yena <yena4882@accugenelab.co.kr>
2020-02-06 17:59:52 +0900

    update

--- src/components/kit/KitContainer.js
+++ src/components/kit/KitContainer.js
@@ -1,11 +1,11 @@
 import React from "react";
-import styles from "./KitContainer.css";
+import "./KitContainer.css";
 
 
 const KitContainer= () => {
   return (
  <div id="hero" className='container'>
-        		<div className="movie-text">검사소개 동영상11111</div>
+        		<div className="movie-text">검사소개 동영상</div>
         			<iframe width="560" height="315" src="https://www.youtube.com/embed/aa_uaZc__c8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     		</div>
   );
